ffd6a11ca6bea69d4b09d13fc10a4504d04a0207
iozbk <165472984+iozbk@users.noreply.github.com>
2026-05-04 14:30:22 +0800

    Update AndroidProducts.mk
    ap2a

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -9,6 +9,6 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/twrp_PQ83A01-trunk_staging.mk
 
 COMMON_LUNCH_CHOICES := \
-    twrp_PQ83A01-trunk_staging-user \
-    twrp_PQ83A01-trunk_staging-userdebug \
-    twrp_PQ83A01-trunk_staging-eng
+    twrp_PQ83A01-ap2a-user \
+    twrp_PQ83A01-ap2a-userdebug \
+    twrp_PQ83A01-ap2a-eng
