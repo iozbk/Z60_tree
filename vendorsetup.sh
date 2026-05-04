9daa97ebdeece4dff4ddb7ad07d60b04f8c5493e
iozbk <165472984+iozbk@users.noreply.github.com>
2026-05-04 13:12:22 +0800

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,7 +5,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-COMMON_LUNCH_CHOICES := \
-    twrp_PQ83A01-user \
-    twrp_PQ83A01-userdebug \
-    twrp_PQ83A01-eng
+add_lunch_combo twrp_PQ83A01-user
+add_lunch_combo twrp_PQ83A01-userdebug
+add_lunch_combo twrp_PQ83A01-eng
