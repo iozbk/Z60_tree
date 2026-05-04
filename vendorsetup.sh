8f3672059f6ab0612612790a7c697f045a455310
iozbk <165472984+iozbk@users.noreply.github.com>
2026-05-04 13:38:52 +0800

    Update vendorsetup.sh
    trunk_staging

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-add_lunch_combo twrp_PQ83A01-trunk-user
-add_lunch_combo twrp_PQ83A01-trunk-userdebug
-add_lunch_combo twrp_PQ83A01-trunk-eng
+add_lunch_combo twrp_PQ83A01-trunk_staging-user
+add_lunch_combo twrp_PQ83A01-trunk_staging-userdebug
+add_lunch_combo twrp_PQ83A01-trunk_staging-eng
