fee3a2701d078904153363772ba3becc7ea0f219
iozbk <165472984+iozbk@users.noreply.github.com>
2026-05-05 19:09:36 +0800

    Update twrp_PQ83A01-ap2a.mk

--- twrp_PQ83A01-ap2a.mk
+++ twrp_PQ83A01-ap2a.mk
@@ -10,10 +10,14 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Omni stuff.
-$(call inherit-product, vendor/twrp/config/common.mk)
+$(call inherit-product, bootable/recovery/config/twrp_common.mk)
 
 # Inherit from PQ83A01 device
 $(call inherit-product, device/nubia/PQ83A01/device.mk)
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.twrp.version=3.7.0 \
+    ro.build.verity=disabled
 
 PRODUCT_DEVICE := PQ83A01
 PRODUCT_NAME := twrp_PQ83A01-ap2a
